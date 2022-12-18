996318901244b130905a204e0a3d1c734945d782
Siful Bappy <bappy.ac.bd@gmail.com>
2022-12-18 15:10:26 +0900

    navbar small hambargur is complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,6 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav className="app-navbar">
-      <div>
         <div className="app-navbar-logo">
           <img src={images.gericht} alt="Logo" />
         </div>
@@ -18,7 +17,15 @@ const Navbar = () => {
           <li className="p-opensans"><a href="#home">Awards</a></li>
           <li className="p-opensans"><a href="#home">Contact</a></li>
         </ul>
-      </div>
+        <div className="app-navbar-login">
+            <a href="#login" className="p-opensans">Login / Register</a>
+            <div></div>
+        <a href="/" className="p-opensans">Book Table</a>
+        </div>
+        <div className="app-navbar-smallscreen" >
+            <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}}></GiHamburgerMenu>
+            
+        </div>
     </nav>
   );
 };
